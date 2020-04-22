e95c062297ec6d141c03f1c2f4e0b8b1b9c319e6
Cyriaque <cyriaquedelaunay@gmail.com>
2020-04-22 09:30:56 +0200

    Added test file

---
+++ test/scripts.ts
@@ -0,0 +1,12 @@
+import SteamScanner from '../src/app';
+import { expect } from 'chai';
+// if you used the '@types/mocha' method to install mocha type definitions, uncomment the following line
+// import 'mocha';
+
+describe('Hello function', () => {
+  it('should return hello world', () => {
+    const Scanner = new SteamScanner();
+    // Scanner.
+    // expect(result).to.equal('Hello World!');
+  });
+});
